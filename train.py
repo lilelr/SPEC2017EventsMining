397242e83ca50b1b9f13a3bf3100dbb59f6411bf
Lele Li <lilelr@163.com>
2018-01-24 21:05:50 +0800

    scale method modify

--- train.py
+++ train.py
@@ -1,9 +1,8 @@
-
 import os
 import pickle
 import numpy as np
 import pandas as pd
-#import catboost
+# import catboost
 from sklearn import preprocessing
 from sklearn import metrics
 from sklearn.ensemble import ExtraTreesClassifier
@@ -12,11 +11,12 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 
+
 class TRAIN(object):
     def __init__(self):
         self.a = None
-        self.data_path = '../data/big_table/'
-        self.algorithm_name = 'DecisionTree'
+        self.data_path = '/Users/yuxiao/Lab/projects/SPEC-CPU2017/508-name-d/namd_r_base.myt-result/'
+        self.algorithm_name = 'it-65-L2-norm'
 
     def sub_train(self):
         algorithm = str(self.algorithm_name + '.csv')
@@ -46,10 +46,10 @@ class TRAIN(object):
         # print(metrics.classification_report(expected, predicted))
         # print(metrics.confusion_matrix(expected, predicted))
 
-        #model = ExtraTreesClassifier()
-        #model.fit(X, Y)
+        # model = ExtraTreesClassifier()
+        # model.fit(X, Y)
         # display the relative importance of each attribute
-        #print(model.feature_importances_)
+        # print(model.feature_importances_)
 
 
         # Build a forest and compute the feature importances
@@ -58,7 +58,7 @@ class TRAIN(object):
                                       random_state=0)
         forest = GradientBoostingClassifier(n_estimators=100, learning_rate=0.1, max_depth=1, random_state=0)
         for _ in range(6):
-            print('the %s th training' % (_+1))
+            print('the %s th training' % (_ + 1))
             assert len(X) == len(y)
             forest.fit(X, y)
             importances = forest.feature_importances_
@@ -69,19 +69,18 @@ class TRAIN(object):
             for f in range(X.shape[1]):
                 print("%d. feature %d  %s (%f)" % (f + 1, indices[f], events_name[indices[f]], importances[indices[f]]))
             X = pd.DataFrame(X)
-            X[X.columns[indices[-10*(_+1):]]] = 0
-            print(X[X.columns[-10*(_+1):-10*(_+1)+1]])
+            X[X.columns[indices[-10 * (_ + 1):]]] = 0
+            print(X[X.columns[-10 * (_ + 1):-10 * (_ + 1) + 1]])
             X = np.array(X)
         return indices
-
 
     def sub_train2(self):
         algorithm = str(self.algorithm_name + '.csv')
         algorithm = os.path.join(self.data_path, algorithm)
         data = pd.read_csv(algorithm)
 
-        X = data.iloc[:, 1:61]
-        y = data.iloc[:, 62]
+        X = data.iloc[:, 1:234]
+        y = data.iloc[:, 235]
         # X = X[:500]
         # y = y[:500]
         events_name = X.columns
@@ -92,7 +91,7 @@ class TRAIN(object):
         Importances = []
         Indices = []
         Events_Name = []
-        Itera = 6
+        Itera = 1
         for _ in range(Itera):
             print('the %s th training' % (_ + 1))
             assert len(X) == len(y)
@@ -104,7 +103,7 @@ class TRAIN(object):
             assert len(predicted) == len(y_test)
             y_test = np.asarray(y_test).astype(float)
             predicted = np.asarray(predicted).astype(float)
-            err = np.mean(np.abs(y_test - predicted)/y_test)
+            err = np.mean(np.abs(y_test - predicted) / y_test)
             importances = forest.feature_importances_
             indices = np.argsort(importances)[::-1]
 
@@ -119,31 +118,31 @@ class TRAIN(object):
                 print("%d. feature %d  %s (%f)" % (f + 1, indices[f], events_name[indices[f]], importances[indices[f]]))
             Events_Name.append(events_Name)
             Importances.append(importanceS)
-            if _ < Itera-1:
+            if _ < Itera - 1:
                 X = pd.DataFrame(X)
-                X[X.columns[indices[-10*(_+1):]]] = 0
-                #print(X[X.columns[-10*(_+1):-10*(_+1)+1]])
+                X[X.columns[indices[-10 * (_ + 1):]]] = 0
+                # print(X[X.columns[-10*(_+1):-10*(_+1)+1]])
                 X = np.array(X)
-            print('Error: ', err*100, '%')
+            print('Error: ', err * 100, '%')
 
         Min_index = Err.index(min(Err))
         for f in range(X.shape[1]):
             print("%d. lowest error feature %d  %s (%f)" % (f + 1, Indices[Min_index][indices[f]],
-                                               events_name[Indices[Min_index][f]], Importances[Min_index][f]))
+                                                            events_name[Indices[Min_index][f]],
+                                                            Importances[Min_index][f]))
         res = {}
         res['result'] = [Err, Indices, Events_Name, Importances]
 
-        output = open('result_'+self.algorithm_name+'.pkl', 'wb')
+        output = open('result_' + self.algorithm_name + '.pkl', 'wb')
         pickle.dump(res, output)
         return res
-
 
     def delete_col(self, data, being_del_col):
         data = data.drop(data.columns[being_del_col])
         return data
 
     def train(self):
-        #being_del_col = self.sub_train()
+        # being_del_col = self.sub_train()
         res = self.sub_train2()
 
         return self.a
